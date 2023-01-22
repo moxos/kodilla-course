b8e1cfc1ae82e7babe51833c97a1e2102484912a
Bartek Blaz <moxosofficial@gmail.com>
2023-01-22 10:06:13 +0100

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(15,7);
+        int resultSubstract = calculator.substract(33, 11);
+
+        System.out.println("Add test");
+        if (resultAdd == 22){
+            System.out.println("Correct");
+        }else {
+            System.out.println("Incorrect");
+        }
+
+        System.out.println("Substract test");
+
+        if (resultSubstract == 22){
+            System.out.println("Correct");
+        }else{
+            System.out.println("Incorrect");
+        }
+
     }
 }
