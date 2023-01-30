784ad116744a4075dbc85bd97695922faa9deaf6
Bartek Blaz <moxosofficial@gmail.com>
2023-01-30 18:28:08 +0100

    7.1 Lambda

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,36 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        PoemBeautifier poemBeautifierSecond= new PoemBeautifier();
+        PoemBeautifier poemBeautifierThird = new PoemBeautifier();
+        poemBeautifier.beautify("START", (Text)-> Text.toLowerCase());
+        poemBeautifierSecond.beautify("pear", (Text)->  Text + " and parsley");
+        poemBeautifierThird.beautify("12345", (Text)-> "Numbers: "+ Text);
+
+        }
+    }
+
