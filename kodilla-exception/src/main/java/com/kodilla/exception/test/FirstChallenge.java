a80598758638518c593de4a77f30bb00048f91e1
Bartek Blaz <moxosofficial@gmail.com>
2023-02-13 18:59:03 +0100

    8.1 Exception

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,26 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+
+    }
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(0, 0);
+            System.out.println(result);
+        }
+        catch (ArithmeticException e){
+           System.out.println("Oh no! Something went wrong! Error: " + e);
+        }finally {
+            System.out.println("I am gonna be here... always!");
+        }
+    }
+}
