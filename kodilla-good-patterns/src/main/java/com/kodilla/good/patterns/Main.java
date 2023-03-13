53a77ebb6aae5dd869f6135c79fd9708343c4147
Bartek Blaz <moxosofficial@gmail.com>
2023-03-13 17:56:28 +0100

    13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String result = movieStore.getMovies().entrySet().stream()
+                .flatMap(title -> title.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+
+        System.out.println(result);
+    }
+}
